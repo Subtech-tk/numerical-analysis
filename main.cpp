220c8b822e096e0c0a2d05ddb52947c59990a3b8
Amanskywalker <ak47su30ac@gmail.com>
2015-10-26 12:43:17 +0530

    new file:   main.cpp 	COPYING 	COPYING.LESSER 	LGIF.cpp 	LGII.cpp 	NDDF 	NDDF.cpp 	NGBI 	NGBI.cpp 	NGFI.cpp 	Numerical Methods alpha build v01.zip 	Numerical Methods alpha build v01/ 	Numerical Methods alpha build v02.zip 	Numerical Methods alpha build v02/ 	Numerical Methods beta build v01/ 	Numerical Methods.cbp 	Numerical Methods.depend 	Numerical Methods.layout 	Numerical_Analysis_a_0_1 	Numerical_Analysis_a_0_2 	Numerical_Analysis_n_0.1 	Numerical_Analysis_n_0_2 	Numerical_Analysis_n_0_3 	Numerical_Analysis_n_0_4 	Numerical_Methods_beta_build_v01.zip 	Readme 	comp_p 	comp_p.cpp 	comp_pb.cpp 	comp_x.cpp 	compute.cpp 	display.cpp 	include/ 	input.txt 	lif.c 	magicsquare.c 	main 	nddf 	numerical integration/ 	powermeth 	powermeth.c 	simpwedd123 	simpwedd123.cpp 	src/

---
+++ main.cpp
@@ -0,0 +1,49 @@
+// start of the project
+#include <iostream>
+#include <stdlib.h>
+#include "NDDF.cpp"
+//#include "compute.cpp"
+#include "NGFI.cpp"
+#include "NGBI.cpp"
+//#include "comp_p.cpp"
+#include "LGIF.cpp"
+//#include "comp_x.cpp"
+#include "LGII.cpp"
+#include "simpwedd123.cpp"
+typedef double Real;
+using namespace std;
+
+// functions in the program
+// for NDDF
+Real comp(Real*,int,Real);
+// for NGFI & NGBI
+int fact(int x);
+Real comp_p(Real,int);
+Real comp_pb(Real,int);
+// for LGFI & LGII
+Real comp_x_a(Real*,Real,int,int);
+Real comp_x_b(Real*,Real,int);
+
+int main()
+{
+    int ch;
+    cout<<"Select a method to Use :\n"
+        <<"1. Newton Divided difference method\n"
+        <<"2. NGFI\n"
+        <<"3. NGBI\n"
+        <<"4. LGFI\n"
+        <<"5. LGII\n"
+        <<"6. Numerical Integration";
+    cin>>ch;
+    switch(ch)
+    {
+        case 1:NDDF_main();break;
+        case 2:NGFI_main();break;
+        case 3:NGBI_main();break;
+        case 4:LGIF_main();break;
+        case 5:LGII_main();break;
+        case 6:num_integration();break;
+        default:;
+    }
+    return 0;
+}
